1229aa0a870023963767bf2c93ca15df670f5433
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-28 18:48:05 -0600

    Clases al header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,99 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+  >
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta
+    name="csrf-token"
+    content="{{ csrf_token() }}"
+  >
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Scripts -->
+  <script
+    src="{{ asset('js/app.js') }}"
+    defer
+  ></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Fonts -->
+  <link
+    rel="dns-prefetch"
+    href="//fonts.gstatic.com"
+  >
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito"
+    rel="stylesheet"
+  >
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link
+    href="{{ asset('css/app.css') }}"
+    rel="stylesheet"
+  >
 </head>
-<body class="bg-grey-200">
-    <div id="app">
-        <nav class="bg-gray-800">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="bg-grey-200 min-h-screen leading-none">
+  <div id="app">
+    <nav class="bg-gray-800 shadow-md py-2">
+      <div class="container mx-auto md:px-0">
+        <div class="flex items-center justify-around">
+          <a
+            class="text-3xl text-white"
+            href="{{ url('/') }}"
+          >
+            {{ config('app.name', 'Laravel') }}
+          </a>
 
-                    </ul>
+          <nav class="flex-1 text-right">
+            <!-- Authentication Links -->
+            @guest
+              <a
+                class="text-white no-underline hover:underline hover:text-gray-300 p-3"
+                href="{{ route('login') }}"
+              >{{ __('Login') }}</a>
+              @if (Route::has('register'))
+                <a
+                  class="text-white no-underline hover:underline hover:text-gray-300 p-3"
+                  href="{{ route('register') }}"
+                >{{ __('Register') }}</a>
+              @endif
+            @else
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+              <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+              <a
+                class="no-underline hover:underline text-gray-300 text-sm p-3"
+                href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+              >
+                {{ __('Logout') }}
+              </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+              <form
+                id="logout-form"
+                action="{{ route('logout') }}"
+                method="POST"
+                class="d-none"
+              >
+                @csrf
+              </form>
+            @endguest
+          </nav>
+        </div>
+      </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <main class="py-4">
+      @yield('content')
+    </main>
+  </div>
 </body>
+
 </html>
