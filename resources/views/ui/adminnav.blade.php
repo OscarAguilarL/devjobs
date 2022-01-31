279783c05ce9f934ca6024e9fb2db90cc6481e79
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-30 20:13:11 -0600

    Crear el formulario para las nuevas vacantes

--- resources/views/ui/adminnav.blade.php
+++ resources/views/ui/adminnav.blade.php
@@ -1,4 +1,8 @@
 <a class="text-white text-sm uppercase font-bold p-3 {{ Request::is('vacantes') ? 'bg-teal-500' : '' }}"
-  href="{{ route('vacantes.index') }}">Ver vacantes</a>
+  href="{{ route('vacantes.index') }}">
+  Ver vacantes
+</a>
 <a class="text-white text-sm uppercase font-bold p-3 {{ Request::is('vacantes/create') ? 'bg-teal-500' : '' }}"
-  href="{{ route('vacantes.create') }}">Nueva vacantes</a>
+  href="{{ route('vacantes.create') }}">
+  Nueva vacantes
+</a>
--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -6,4 +6,25 @@
 
 @section('content')
   <h1 class="text-2xl text-center mt-5">Nueva Vacante</h1>
+
+  <form class="max-w-lg mx-auto my-10">
+    <div class="mb-5">
+      <label
+        for="titulo"
+        class="block text-gray-700 text-sm mb-2"
+      >Titulo de la vacante:</label>
+      <input
+        id="titulo"
+        type="text"
+        class="p-3 bg-gray-100 rounded-sm form-input w-full @error('titulo') border-red-500 border @enderror"
+        name="titulo"
+        value="{{ old('titulo') }}"
+      >
+    </div>
+
+    <button
+      type="submit"
+      class="bg-teal-500 hover:bg-teal-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline rounded-lg"
+    >Publicar vacante</button>
+  </form>
 @endsection
