f16aa2ad7c46982240e14f927e973e1f87f5355c
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-02 23:18:24 -0600

    Agregando o eliminando habilidades

--- public/js/app.js
+++ public/js/app.js
@@ -1908,6 +1908,22 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+//
+//
+//
+//
 //
 //
 //
@@ -1922,8 +1938,29 @@ __webpack_require__.r(__webpack_exports__);
   props: {
     skills: Array
   },
+  data: function data() {
+    return {
+      habilidades: new Set()
+    };
+  },
   mounted: function mounted() {
     console.log(this.skills);
+  },
+  methods: {
+    handleClick: function handleClick(e) {
+      if (e.target.classList.contains('bg-teal-500')) {
+        e.target.classList.remove('bg-teal-500');
+        this.habilidades["delete"](e.target.textContent);
+      } else {
+        e.target.classList.add('bg-teal-500');
+        this.habilidades.add(e.target.textContent);
+      } // agregar las habilidades al input hidden
+
+
+      var stringHabilidades = _toConsumableArray(this.habilidades);
+
+      this.$refs.inputSkills.value = stringHabilidades;
+    }
   }
 });
 
@@ -37515,22 +37552,30 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "ul",
-    { staticClass: "flex flex-wrap justify-center" },
-    _vm._l(this.skills, function (skill) {
-      return _c(
-        "li",
-        {
-          key: skill,
-          staticClass:
-            "border border-gray-500 px-10 py-3 mb-3 rounded mr-4 cursor-pointer",
-        },
-        [_vm._v(_vm._s(skill))]
-      )
+  return _c("div", [
+    _c(
+      "ul",
+      { staticClass: "flex flex-wrap justify-center" },
+      _vm._l(this.skills, function (skill) {
+        return _c(
+          "li",
+          {
+            key: skill,
+            staticClass:
+              "border border-gray-500 px-10 py-3 mb-3 rounded mr-4 cursor-pointer",
+            on: { click: _vm.handleClick },
+          },
+          [_vm._v(_vm._s(skill))]
+        )
+      }),
+      0
+    ),
+    _vm._v(" "),
+    _c("input", {
+      ref: "inputSkills",
+      attrs: { type: "hidden", name: "skills", id: "skills" },
     }),
-    0
-  )
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/components/ListaSkills.vue
+++ resources/js/components/ListaSkills.vue
@@ -1,11 +1,15 @@
 <template>
-  <ul class="flex flex-wrap justify-center">
-    <li
-      class="border border-gray-500 px-10 py-3 mb-3 rounded mr-4 cursor-pointer"
-      v-for="(skill) in this.skills"
-      :key="skill"
-    >{{ skill }}</li>
-  </ul>
+  <div>
+    <ul class="flex flex-wrap justify-center">
+      <li
+        class="border border-gray-500 px-10 py-3 mb-3 rounded mr-4 cursor-pointer"
+        v-for="(skill) in this.skills"
+        :key="skill"
+        @click="handleClick"
+      >{{ skill }}</li>
+    </ul>
+    <input type="hidden" name="skills" id="skills" ref="inputSkills" />
+  </div>
 </template>
 
 <script>
@@ -13,8 +17,26 @@ export default {
   props: {
     skills: Array
   },
+  data: () => ({
+    habilidades: new Set()
+  }),
   mounted () {
     console.log(this.skills);
+  },
+  methods: {
+    handleClick (e) {
+      if (e.target.classList.contains('bg-teal-500')) {
+        e.target.classList.remove('bg-teal-500')
+        this.habilidades.delete(e.target.textContent)
+      } else {
+        e.target.classList.add('bg-teal-500')
+        this.habilidades.add(e.target.textContent)
+      }
+
+      // agregar las habilidades al input hidden
+      const stringHabilidades = [...this.habilidades]
+      this.$refs.inputSkills.value = stringHabilidades
+    }
   }
 }
 </script>
