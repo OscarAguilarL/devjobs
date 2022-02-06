d6b2dc7488a373fc06a714f40aea80865f53e5c3
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-06 17:58:52 -0600

    Guardando una vacante en la base de datos

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -26,7 +26,6 @@ class VacanteController extends Controller
      */
     public function index()
     {
-        //
         return view('vacantes.index');
     }
 
@@ -70,7 +69,20 @@ class VacanteController extends Controller
             'skills' => 'required',
         ]);
 
-        return 'desde store';
+        // almacenar en la BD
+
+        auth()->user()->vacantes()->create([
+            'titulo' => $data['titulo'],
+            'imagen' => $data['imagen'],
+            'descripcion' => $data['descripcion'],
+            'skills' => $data['skills'],
+            'categoria_id' => $data['categoria'],
+            'experiencia_id' => $data['experiencia'],
+            'ubicacion_id' => $data['ubicacion'],
+            'salario_id' => $data['salario'],
+        ]);
+
+        return redirect()->action('VacanteController@index');
     }
 
     /**
@@ -132,7 +144,7 @@ class VacanteController extends Controller
     // Borrar imagen via ajax
     public function borrarimagen(Request $request)
     {
-        if ($request->ajax()){
+        if ($request->ajax()) {
             $imagen = $request->get('imagen');
 
             if (File::exists('storage/vacantes/' . $imagen)) {
