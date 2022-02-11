cfe19c6932fb8c450a6996d2b293b3b0f570972b
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-10 19:08:43 -0600

    Crendo una pagina para ver las notificaciones

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     // Subir imágenes
     Route::post('/vacantes/imagen', 'VacanteController@imagen')->name('vacantes.imagen');
     Route::post('/vacantes/borrarimagen', 'VacanteController@borrarimagen')->name('vacantes.borrar');
+
+    // Notificaciones
+    Route::get('/notificaciones', 'NotificationesController')->name('notificaciones');
 });
 
 // Enviar datos para una vacante
